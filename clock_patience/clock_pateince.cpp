a9de47296c83430754f4f44a6a488710494c0b72
cSg <craig.greenberg@nist.gov>
2013-01-10 17:51:50 -0500

    tmp

--- clock_patience/clock_pateince.cpp
+++ clock_patience/clock_pateince.cpp
@@ -5,18 +5,19 @@
 using namespace std;
 
 class Card{
+private:
   map <string, int> rank_2_pos;
   string suit;
   string rank;
 public:
-  Card(string, string);
+  Card(char, char);
   string get_suit();
   string get_rank();
   int get_pos();
   bool is_down;
 };
 
-Card::Card(string r, string s){
+Card::Card(char r, char s){
   rank = r;
   suit = s;
   rank_2_pos["1"] = 1;
@@ -32,6 +33,7 @@ Card::Card(string r, string s){
   rank_2_pos["Q"] = 11;
   rank_2_pos["K"] = 12;
   rank_2_pos["A"] = 13;
+  is_down = true;
 }
 
 string Card::get_suit(){
@@ -46,20 +48,41 @@ int Card::get_pos(){
   return rank_2_pos[rank];
 }
 
-bool play_game(){
-  string rs;
-  for (int i=0; i<52; i++){
-    cin >> rs;
-    if (rs.compare("#") == 0){
-      return false;
-    }
-    cout << i << rs << endl;
-  }
+class Table{
+private:
+   vector< vector<Card*> > piles;
+public:
+  void setup_game(vector<string>);
+  Table(): piles(13){};
+};
+
+void Table::setup_game(vector<string> rank_suits){
+  //Card a_card(rs[0], rs[1]);
+  //table[( 12 - (i % 13) )].push_back(&a_card);
+  for (vector<string>::iterator i)
 }
+
+bool play_game(){
+  // Setup table
+  string rs;
+  
+ // Play game
+  //cout << table[0][0]->get_rank()<< endl;
+}
+
 int main(){
   while (cin){
-    if (not play_game()){
-      break;
+    Table a_table;
+    vector<string> rank_suits;
+    string rs;
+    for (int i=0; i<52; i++){
+      cin >> rs;
+      if (rs.compare("#") == 0){
+        return false;
+      }
+      rank_suits.push_back(rs);
     }
+    a_table.setup_game(rank_suits);
   }
 }
+
