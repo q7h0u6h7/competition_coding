1e0ff1e524d65bb1b874b681f8509847102e1f7b
cSg <craig.greenberg@nist.gov>
2013-01-30 18:54:36 -0500

    new way of reading line of ints

--- box_of_bricks/box_of_bricks.cpp
+++ box_of_bricks/box_of_bricks.cpp
@@ -10,22 +10,28 @@ using namespace std;
 int main(){
   int num_bins;
   int num_per_bin;
-  //vector<int> bins;
+  vector<int> bins;
   string line;
+  string tmp;
   while (cin){
     cin >> num_bins;
+    getline(cin, tmp);
     if (!num_bins){
       break;
     }
     getline(cin, line);
     istringstream is(line);
     vector<int> bins(istream_iterator<int>(is),
-                     istream_iterator<int>());
-    for (vector<int>::iterator it = bins.begin();
-         it != bins.end();
-         ++it){
-      cout << *it << endl;
-    }
-    //num_per_bin = accumulate(bins.begin(), bins.end(), 0);
+                     (istream_iterator<int>()) ); //extra parens necessary to prevent compiler from considering this a function
+//     for (vector<int>::iterator it = bins.begin();
+//          it != bins.end();
+//          ++it){
+//       cout << *it;
+//     }
+//     cout << endl;
+    cout << line << endl;
+    num_per_bin = accumulate(bins.begin(), bins.end(), 0);
+    cout << num_per_bin << endl;
+
   }
 }
