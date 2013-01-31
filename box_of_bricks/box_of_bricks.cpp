35d2d1ac58e0637f816390d58a3ded782ed7c66f
cSg <craig.greenberg@nist.gov>
2013-01-31 12:42:45 -0500

    submitted version

--- box_of_bricks/box_of_bricks.cpp
+++ box_of_bricks/box_of_bricks.cpp
@@ -3,7 +3,7 @@
 #include <iterator>
 #include <vector>
 #include <numeric>
-
+#include <cmath> //note that there is math.h and cmath and how this works is compiler dependent
 
 using namespace std;
 
@@ -12,26 +12,28 @@ int main(){
   int num_per_bin;
   vector<int> bins;
   string line;
-  string tmp;
+  string _;
+  int num_bricks;
+  int set_num = 0;
   while (cin){
     cin >> num_bins;
-    getline(cin, tmp);
+    getline(cin, _); // throw away newline
     if (!num_bins){
       break;
     }
+    ++set_num;
     getline(cin, line);
     istringstream is(line);
     vector<int> bins(istream_iterator<int>(is),
                      (istream_iterator<int>()) ); //extra parens necessary to prevent compiler from considering this a function
-//     for (vector<int>::iterator it = bins.begin();
-//          it != bins.end();
-//          ++it){
-//       cout << *it;
-//     }
-//     cout << endl;
-    cout << line << endl;
-    num_per_bin = accumulate(bins.begin(), bins.end(), 0);
-    cout << num_per_bin << endl;
-
+    num_per_bin = accumulate(bins.begin(), bins.end(), 0) / num_bins;
+    num_bricks = 0;
+    for (vector<int>::iterator it = bins.begin();
+         it != bins.end();
+         ++it){
+      num_bricks += std::abs(*it - num_per_bin);
+    }
+    cout << "Set #" << set_num << endl;
+    cout << "The minimum number of moves is " << num_bricks / 2 << "." << endl << endl; // Note the 2nd endl due to "output empty line after each set."
   }
 }
